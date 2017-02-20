04bec0ce668151c73b03aa2d2a4a9c495c4c7f80
xuqingfeng <js-xqf@hotmail.com>
2017-02-20 16:31:41 +0800

    Fix ignore cmd/gotpl

---
+++ cmd/gotpl/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/xuqingfeng/gotpl/util"
+)
+
+func main() {
+
+	data := util.Data{}
+	flag.StringVar(&data.Author, "author", "author", "author of the porject")
+	flag.StringVar(&data.Repo, "repo", "repo", "repository name")
+
+	flag.Parse()
+
+	err := util.CreateDirectory(data)
+	if err != nil {
+		log.Fatalf("E! %v", err)
+	}
+	newFs, err := util.Feed(data)
+	if err != nil {
+        log.Fatalf("E! %v", err)
+	}
+	err = util.CreateFile(data, newFs)
+	if err != nil {
+        log.Fatalf("E! %v", err)
+	}
+}
--- docs.go
+++ docs.go
@@ -1,2 +1,2 @@
-// package gotpl include docs
+// demo documents
 package gotpl
